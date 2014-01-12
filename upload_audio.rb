3bc405beb5218a8dd425f1ab284437a150148e5a
Gaston Ramos <ramos.gaston@gmail.com>
2014-01-12 14:47:33 -0300

    some refactoring and add convertion to ogg

--- upload_audio.rb
+++ upload_audio.rb
@@ -2,25 +2,29 @@ require 'date'
 
 program_name = ARGV[0]
 time         = DateTime.now.to_s.tr(":","")
-title        = "#{program_name}_#{time}"
-filename     = "#{title}.mp3"
-rec_cmd      = "rec -r 44100 -C 48.01 -c 2 #{filename} trim 0 00:00:10"
+filename     = "#{program_name}_#{time}"
+rec_cmd      = "rec -r 44100 -C 48.01 -c 2 #{filename} trim 0 02:20:00"
 
 # --------------------------------------------
 # rec the audio
 puts "==> [ rec ] #{filename}..."
 system rec_cmd
 
+upload "#{filename}.mp3"
+upload "#{filename}.ogg"
+
+def mp3_to_ogg(filename)
+  "mpg321 #{filename}.mp3 -w raw && oggenc raw -o #{filename}.ogg"
+end
+
 # --------------------------------------------
 # upload an audio file
-puts "==> [ upload ] #{filename} to gastonramos.com.ar/vocesdelacosta/..."
+def upload(filename)
+  puts "==> [ upload ] #{filename} to gastonramos.com.ar/vocesdelacosta/..."
+  
+  server_path = "~/public_html/gastonramos.com.ar/vocesdelacosta/programas-grabados/"
+  upload_cmd  = "scp #{filename} gaston@gastonramos.com.ar:#{server_path}"
+  system upload_cmd
+end
 
-server_path = "~/public_html/gastonramos.com.ar/vocesdelacosta/audios/"
-
-upload_cmd = "scp #{filename} gaston@gastonramos.com.ar:#{server_path}"
-
-system upload_cmd
-# --------------------------------------------
-# print track link
-puts "http://gastonramos.com.ar/vocesdelacosta/audios/#{filename}"
 
