531368b551e1bdd82b21b1bc04f8b3cf906a38ee
Gaston Ramos <ramos.gaston@gmail.com>
2014-03-15 06:39:21 -0300

    fix upload script

--- upload_audio.rb
+++ upload_audio.rb
@@ -1,9 +1,19 @@
 require 'date'
 
+# --------------------------------------------
+# upload an audio file
+def upload(filename)
+  puts "==> [ upload ] #{filename} to gastonramos.com.ar/vocesdelacosta/..."
+  
+  server_path = "~/public_html/gastonramos.com.ar/vocesdelacosta/programas-grabados/"
+  upload_cmd  = "scp #{filename} gaston@gastonramos.com.ar:#{server_path}"
+  system upload_cmd
+end
+
 program_name = ARGV[0]
 time         = DateTime.now.to_s.tr(":","")
 filename     = "#{program_name}_#{time}"
-rec_cmd      = "rec -r 44100 -C 48.01 -c 2 #{filename} trim 0 02:20:00"
+rec_cmd      = "rec -r 44100 -C 48.01 -c 2 #{filename}.mp3 trim 0 02:20:00"
 
 # --------------------------------------------
 # rec the audio
@@ -17,14 +27,5 @@ def mp3_to_ogg(filename)
   "mpg321 #{filename}.mp3 -w raw && oggenc raw -o #{filename}.ogg"
 end
 
-# --------------------------------------------
-# upload an audio file
-def upload(filename)
-  puts "==> [ upload ] #{filename} to gastonramos.com.ar/vocesdelacosta/..."
-  
-  server_path = "~/public_html/gastonramos.com.ar/vocesdelacosta/programas-grabados/"
-  upload_cmd  = "scp #{filename} gaston@gastonramos.com.ar:#{server_path}"
-  system upload_cmd
-end
 
 
