83d2df1b60ae3ff0fbb240dc3db6422c155495e1
Gaston Ramos <ramos.gaston@gmail.com>
2014-04-16 11:30:00 -0300

    add crons list

--- sync_web.sh
+++ sync_web.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync --archive --verbose --recursive web/ gaston@vocesdelacosta.com.ar:~/public_html/vocesdelacosta.com.ar/
+rsync --archive --verbose --recursive --delete web/ gaston@vocesdelacosta.com.ar:~/public_html/vocesdelacosta.com.ar/
