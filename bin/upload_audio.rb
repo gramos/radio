523ad8182f610289564700383adb8d2b5d857e5b
Gaston Ramos <ramos.gaston@gmail.com>
2014-04-16 11:35:35 -0300

    move upload audio script to bin

---
+++ bin/upload_audio.rb
@@ -0,0 +1,20 @@
+require 'date'
+
+program_name = ARGV[0]
+duration     = ARGV[1]
+time         = DateTime.now.to_s.tr(":","")
+filename     = "#{program_name}_#{time}"
+rec_cmd      = "rec -r 44100 -C 48.01 -c 2 #{filename}.mp3 trim 0 #{duration}"
+
+def mp3_to_ogg(filename)
+  "mpg321 #{filename}.mp3 -w raw && oggenc raw -o #{filename}.ogg"
+end
+
+# --------------------------------------------
+# rec the audio
+puts "==> [ rec ] #{filename}..."
+system rec_cmd
+system mp3_to_ogg filename
+system "rm raw"
+system "mv #{filename}.mp3 web/programas-grabados/"
+system "mv #{filename}.ogg web/programas-grabados/"
