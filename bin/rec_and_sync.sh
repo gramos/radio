3cd90faeb156c9dee90ba55268bad85dd1e4d403
Gaston Ramos <ramos.gaston@gmail.com>
2014-09-09 17:26:05 -0300

    Some fixes in rec script.

--- bin/rec_and_sync.sh
+++ bin/rec_and_sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 date  >> log/vocesdelacosta.log 
 echo "grabando $1"  >> log/vocesdelacosta.log 
-ruby upload_audio.rb $1  >> log/vocesdelacosta.log 2>&1
+ruby ./bin/upload_audio.rb $1 $2 >> log/vocesdelacosta.log 2>&1
 
 date >> log/vocesdelacosta.log 
 echo "generando la web"  >> log/vocesdelacosta.log 
@@ -11,4 +11,4 @@ ruby gen.rb  >> ../log/vocesdelacosta.log 2>&1
 cd .. 
 date  >> log/vocesdelacosta.log
 echo "subiendo el sitio" >> log/vocesdelacosta.log 
-./sync_web.sh  >> log/vocesdelacosta.log 2>&1
+./bin/sync_web.sh  >> log/vocesdelacosta.log 2>&1
