f46cb7737d9fe3299555db44816ef0ac195d5cb5
Gaston Ramos <ramos.gaston@gmail.com>
2014-09-09 17:09:44 -0300

    Use lame to convert to mp3

--- bin/upload_audio.rb
+++ bin/upload_audio.rb
@@ -5,7 +5,7 @@ duration     = ARGV[1] || '7200'
 time         = DateTime.now.to_s.tr(":","")
 filename     = "#{program_name}_#{time}"
 rec_cmd      = "radio -f '98.5' & arecord -q -c 2 -D hw:2,0 -r 96000 -f s16_LE" +
-               " -d #{duration} > #{filename}.mp3"
+               " -d #{duration} | lame -r -s 22.05 -m m -b 64 - #{filename}.mp3"
 
 def mp3_to_ogg(filename)
   "mpg321 #{filename}.mp3 -w raw && oggenc raw -o #{filename}.ogg"
@@ -17,6 +17,5 @@ end
 puts "==> [ record ] #{filename}..."
 system rec_cmd
 system mp3_to_ogg filename
-system "rm raw"
 system "mv #{filename}.mp3 web/programas-grabados/"
 system "mv #{filename}.ogg web/programas-grabados/"
